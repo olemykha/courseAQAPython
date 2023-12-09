9134a3dbd7b18e77384e4fb3f115291e09eda189
olemykha <alex.mkhlk@gmail.com>
2023-12-09 16:58:59 +0100

    add homework2 07.12.23

---
+++ homework/homework071223.py
@@ -0,0 +1,24 @@
+"""HOMEWORK2."""
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s')
+
+# Example %-format from teacher
+norway_text = 'Automatisering akselererer %syeblikket da roboter vil erobre planeten v%sr. (%s)' % ('ø', 'å', 'Æ')
+logging.info(norway_text)
+
+norwegian_letter_ue = 'ø'
+norwegian_letter_o = 'å'
+norwegian_letter_eh = 'Æ'
+
+# Using str.format()
+norway_text_with_format_call = (
+    'Automatisering akselererer {0}yeblikket da roboter vil erobre planeten v{1}r. ({2})'
+).format(norwegian_letter_ue, norwegian_letter_o, norwegian_letter_eh)
+logging.info(norway_text_with_format_call)
+
+# Using f-string
+norway_text_with_fstring_format = (
+    f'Automatisering akselererer {norwegian_letter_ue}yeblikket da roboter vil erobre planeten v{norwegian_letter_o}r. ({norwegian_letter_eh})'
+)
+logging.info(norway_text_with_fstring_format)
