6f58f1a9c849ac5abb52b0e688d36dce52f14946
olemykha <alex.mkhlk@gmail.com>
2023-12-10 11:55:27 +0100

    fixed issues with E501 errors for homework2 07.12.23

--- homework/homework071223.py
+++ homework/homework071223.py
@@ -1,10 +1,16 @@
 """HOMEWORK2."""
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s: %(message)s',
+)
 
 # Example %-format from teacher
-norway_text = 'Automatisering akselererer %syeblikket da roboter vil erobre planeten v%sr. (%s)' % ('ø', 'å', 'Æ')
+norway_text = (
+    'Automatisering akselererer %syeblikket da '
+    'roboter vil erobre planeten v%sr. (%s)' % ('ø', 'å', 'Æ')
+)
 logging.info(norway_text)
 
 norwegian_letter_ue = 'ø'
@@ -13,12 +19,15 @@ norwegian_letter_eh = 'Æ'
 
 # Using str.format()
 norway_text_with_format_call = (
-    'Automatisering akselererer {0}yeblikket da roboter vil erobre planeten v{1}r. ({2})'
+    'Automatisering akselererer {0}yeblikket da '
+    'roboter vil erobre planeten v{1}r. ({2})'
 ).format(norwegian_letter_ue, norwegian_letter_o, norwegian_letter_eh)
 logging.info(norway_text_with_format_call)
 
 # Using f-string
 norway_text_with_fstring_format = (
-    f'Automatisering akselererer {norwegian_letter_ue}yeblikket da roboter vil erobre planeten v{norwegian_letter_o}r. ({norwegian_letter_eh})'
+    f'Automatisering akselererer {norwegian_letter_ue}yeblikket da '
+    f'roboter vil erobre planeten v{norwegian_letter_o}r. '
+    f'({norwegian_letter_eh})'
 )
 logging.info(norway_text_with_fstring_format)
