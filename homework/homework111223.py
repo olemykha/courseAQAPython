cf49d7af73333aee75d655f34b3ff719d1da2d02
olemykha <alex.mkhlk@gmail.com>
2023-12-13 21:28:49 +0100

    fix some bugs from homework4

--- homework/homework111223.py
+++ homework/homework111223.py
@@ -1,4 +1,22 @@
-"""HOMEWORK4."""
+"""
+HOMEWORK4.
+
+Task Description:
+-----------------
+HOMEWORK4, involves the following tasks:
+
+Task 1. Need to count each vowel occurrence in the text
+(total sum of lower and capital cases).
+List of vowels: A, E, I, O, and U.
+The text from the teacher is provided in the poem_text variable.
+
+Task 2. Need to modify the text (which is specified in the poem_text variable),
+where each vowel is replaced with:
+A->À,a->à; E->É,e->é; I->Í,i->í; O->Ó,o->ó; U->Ú,u->ú.
+and print it.
+
+"""
+
 import logging
 
 logging.basicConfig(
@@ -23,7 +41,7 @@ Ten thousand saw I at a glance,
 Tossing their heads in sprightly dance.
 """
 
-poem_text.lower()
+poem_text = poem_text.lower()
 vowels = ['a', 'e', 'i', 'o', 'u']  # list of vowels
 
 separator_number = 18
@@ -31,7 +49,7 @@ logging.info('-' * separator_number)
 logging.info(f"| {'Vowel':^2} | {'Count':^6} |")
 logging.info('-' * separator_number)
 
-# Counting the number of vowels in a text
+# Task 1. Counting the number of vowels in a text
 for vowel in vowels:
     count = poem_text.count(vowel)
     logging.info(f'| {vowel:^6}|  {count:^5} |')
@@ -39,13 +57,13 @@ for vowel in vowels:
 logging.info('-' * separator_number)
 logging.info('The table output is complete.')
 
-# The second task is to change all the vowels to those provided by the teacher.
-modified_poem_text = (
-    poem_text
-    .replace('A', 'À').replace('a', 'à')
-    .replace('E', 'É').replace('e', 'é')
-    .replace('I', 'Í').replace('i', 'í')
-    .replace('O', 'Ó').replace('o', 'ó')
-    .replace('U', 'Ú').replace('u', 'ú')
-)
+# Task 2. Changing all the vowels to those provided by the teacher.
+new_symbols_for_replacement = str.maketrans({
+    'A': 'À', 'a': 'à',
+    'E': 'É', 'e': 'é',
+    'I': 'Í', 'i': 'í',
+    'O': 'Ó', 'o': 'ó',
+    'U': 'Ú', 'u': 'ú',
+})
+modified_poem_text = poem_text.translate(new_symbols_for_replacement)
 logging.info(f'Modified text of the poem: {modified_poem_text}')
